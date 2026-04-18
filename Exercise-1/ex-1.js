cd946d1308753e4a493544fa499fb9a5ba1cee9d
fatima hassan <fatimahasan774@gmail.com>
2026-04-18 07:54:18 +0300

    send exercise 3

--- Exercise-1/ex-1.js
+++ Exercise-1/ex-1.js
@@ -1,12 +1,27 @@
 let x=15;
 let y="15";
-console.log(x == y);
-console.log(x != y);
-console.log(x === y);
-console.log(x > y);
-console.log(x < y);
-console.log(x >= y);
-console.log(x <= y);
+// console.log(x == y);
+// console.log(x != y);
+// console.log(x === y);
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x >= y);
+// console.log(x <= y);
+
+
+
+// logic operators
+
+let isSanny =true;
+let weekend =true;
+
+console.log(isSanny && weekend);
+
+// console.log(isSanny || weekend);
+
+
+// console.log(!weekend);
+
 
 
 
